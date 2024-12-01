d04db26a18e6451896ad75d84933bc31411b18e8
Sedhu Madhavan <sedhumadhavan0002@gmail.com>
2024-12-01 11:22:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const dotenv = require('dotenv')
 const mongoose = require('mongoose')
-dotenv.config({path:'./config.env'})
+dotenv.config()
 const app = require('./app')
 
 const DB = process.env.DATABASE.replace("<DATABASE_PASSWORD>", process.env.PASSWORD)
 mongoose.connect(DB).then(()=> console.log("DB connected successfully!")).catch((err) => console.log(err))
 
 const port = 8000
-app.listen(port,'localhost', () =>{
+app.listen(port,'0.0.0.0', () =>{
     console.log('server starts listening..')
 })
 
