3ee61cd516785a446a1739a8bc4f42d8fc93123e
Sedhu Madhavan <sedhumadhavan0002@gmail.com>
2024-12-01 11:41:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.json())
 app.use(cookieParser());
 
 app.use(cors({
-    origin: 'http://localhost:3000',  
+    origin: 'https://world-memo.netlify.app',  
     credentials: true,
     methods: ['GET', 'POST', 'DELETE','PATCH']
   }));
